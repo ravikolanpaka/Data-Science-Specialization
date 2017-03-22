0156dc5c21e93339e8deaf46dec00d7a37018068
rehab-ahmed <rehab.ahmed@eng.asu.edu.eg>
2017-03-22 14:37:27 +0200

    final

--- Exploratory Data Analysis/Basic R Plotting/plot3.R
+++ Exploratory Data Analysis/Basic R Plotting/plot3.R
@@ -2,7 +2,7 @@ dat <- read.table("household_power_consumption.txt", sep=";", header=TRUE, na.st
 dat_slice <- dat[dat$Date %in% c("1/2/2007", "2/2/2007"),]
 dat_slice$Date <- as.Date(dat_slice$Date, format = "%d/%m/%Y ")
 dat_slice$newdate <- with(dat_slice, as.POSIXct(paste(Date, Time), format="%Y-%m-%d %H:%M"))
-
+png("plot3.png")
 plot(dat_slice$newdate,dat_slice$Sub_metering_1, 
      type="n",
      ylab = "Energy sub metering",
@@ -18,5 +18,4 @@ lines(dat_slice$newdate,dat_slice$Sub_metering_3, type="l", col="blue")
 
 legend("topright", lty=c(1,1,1), col = c("black","blue", "red"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-dev.copy(png, "plot3.png")
 dev.off()
